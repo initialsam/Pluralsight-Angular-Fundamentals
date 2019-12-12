809888341c5529e2f3cceece0d42f652ad4c6606
sam <evaoosyz@gmail.com>
2019-12-12 22:36:45 +0800

    Creating and Communicating Between Angular Components Done

---
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'events-list',
+  template: `
+  <div>
+    <h1>Upcoming Angular Events</h1>
+    <hr/>
+    <div class="well">
+      <div>EventsListComponent</div>
+    </div>
+    <event-thumbnail #thumbnail
+    (eventClick) = "handleEventClicked($event)"
+    [event]="event1"></event-thumbnail>
+    <h3>{{thumbnail.somep}}</h3>
+    <button class="btn btn-primary" (click)="thumbnail.logFoo()">Log me some foo</button>
+  </div>
+  `,
+  styles: [`
+    .well div { color: red; }
+  `]
+})
+export class EventsListComponent {
+  event1 = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '9/26/2036',
+    time: '10:00 am',
+    price: 599.99,
+    imageUrl: '/assets/images/angularconnect-shield.png',
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'England'
+    }
+  }
+  handleEventClicked(data){
+    console.log('received:',data);
+
+  }
+}
