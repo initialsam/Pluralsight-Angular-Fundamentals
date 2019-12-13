c79dc00e552729921c09ea3980539f7ca3eadc5e
sam <evaoosyz@gmail.com>
2019-12-13 11:14:53 +0800

    Exploring the Angular Template Syntax Done

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -3,22 +3,31 @@ import { Component, Input, Output,EventEmitter } from '@angular/core'
 @Component({
   selector: 'event-thumbnail',
   template: `
-   <div class="well">
-      <div>EventThumbnailComponent</div>
-    </div>
     <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
-      <div>
+      <h2 [ngStyle]="getNameStyle()"> {{event?.name}}</h2>
+      <div [ngClass]="getDateCalss()" >Date: {{event?.date}}</div>
+      <div [ngSwitch]="event?.time" >
+      Time: {{event?.time}}
+      <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+      <span *ngSwitchCase="'9:00 am'">(Late Start)</span>
+      <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      <div>Price: \${{event?.price}}</div>
+      <div *ngIf="event?.location">
         <span>Location: {{event.location.address}}</span>
         <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
       </div>
-      <button class="btn btn-primary" (click)="handleClickMe()">Click me</button>
+      <div [hidden]="!event?.onlineUrl">
+        online Url: {{event.onlineUrl}}
+      </div>
+      <!--<button class="btn btn-primary" (click)="handleClickMe()">Click me</button>-->
     </div>
   `,
   styles: [`
+    .green {color:#80fb80 !important;}
+    .bold {font-weight:bold;}
+    .thumbnail {min-height:210px}
+    .thumbnail {min-height:210px}
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
   `]
@@ -28,6 +37,18 @@ export class EventThumbnailComponent {
   @Output() eventClick = new EventEmitter()
   handleClickMe(){
     this.eventClick.emit(this.event.name);
+  }
+  getDateCalss(){
+    let isMyDate = this.event && this.event.date === '4/15/2037';
+    return {green :isMyDate,bold:isMyDate }
+    //return 'green bold'
+    //return ['green','bold']
+  }
+
+  getNameStyle():any{
+    if(this.event && this.event.name === 'ng-conf 2037')
+      return {color :'#e2e674','font-weight':'bold' }
+    return {}
   }
   logFoo(){
     console.log('foo');
