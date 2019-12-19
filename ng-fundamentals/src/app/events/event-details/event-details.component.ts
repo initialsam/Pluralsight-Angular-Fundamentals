bccc4f25a4dc717e3237b24afd0d466970d5c4e3
sam <evaoosyz@gmail.com>
2019-12-19 18:33:02 +0800

    Bug fix : snapshot 不會偵測到id改變

--- ng-fundamentals/src/app/events/event-details/event-details.component.ts
+++ ng-fundamentals/src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
-import { EventService,IEvent,ISession } from '../shared/index'
+import { ActivatedRoute, Params } from '@angular/router'
+import { EventService, IEvent, ISession } from '../shared/index'
 
 @Component({
   selector: 'event-details',
@@ -11,20 +11,25 @@ import { EventService,IEvent,ISession } from '../shared/index'
   `]
 })
 export class EventDetailsComponent {
-  event:IEvent
-  addMode:boolean
+  event: IEvent
+  addMode: boolean
   _filterBy: string = 'all';
   _sortBy: string = 'votes';
-  
-  constructor(private eventService:EventService, private route: ActivatedRoute) {}
+
+  constructor(private eventService: EventService, private route: ActivatedRoute) { }
   ngOnInit() {
-    this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
+    //snapshot 不會監看id有沒有改變
+    //this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
+    this.route.params.forEach((params: Params) => {
+      this.event = this.eventService.getEvent(+params['id']);
+      this.addMode = false;
+    })
   }
   addSession() {
     this.addMode = true
   }
 
-  saveNewSession(session:ISession) {
+  saveNewSession(session: ISession) {
     const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id));
     session.id = nextId + 1
     this.event.sessions.push(session)
